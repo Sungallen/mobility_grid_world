9bdeb6ce16bac973ecae76b8370834b69f65d2d0
宋宇倫 <allen@dhcp-10-29-219-120.dyn.mit.edu>
2025-10-01 12:43:27 -0400

    Updates

---
+++ _next/static/OuBt5BGAtB6OLziypYt1l/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/c03a3037953d56ff.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/5e525a124b8b7eb4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/519be82351ed4af3.js",
+  "static/chunks/turbopack-0fb6d2606bc5927e.js"
+])
